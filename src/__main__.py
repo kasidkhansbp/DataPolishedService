c04b745b8f17a41fd01d00750e4b6d6da772b014
kasid Khan <kasidkhan@gmail.com>
2024-12-08 11:20:20 +0100

    removed unused import

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,8 @@
+
 from data_processing.clean_scripts import clean_text
 from langchain_utils.llm_scripts import generate_structured_data
 from storage.dynamodb_builder import DynamoDBBuilder
 from storage.s3_scripts import read_s3_bucket
-import json
 
 def main():
     text = read_s3_bucket("mybucket-qwertyui","unstructuredData.txt")
