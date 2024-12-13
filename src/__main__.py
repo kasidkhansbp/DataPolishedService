afc15ed6fa4f59a6ba74be99412b80d26d643335
kasid Khan <kasidkhan@gmail.com>
2024-12-13 08:54:20 +0100

    removed unused imports

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,8 @@
 
-from data_processing.clean_scripts import clean_text
-from langchain_utils.llm_scripts import generate_structured_data
-from storage.dynamodb_builder import DynamoDBBuilder
-from storage.s3_scripts import read_s3_bucket
+from module.common_utilities import CommonUtilities
 
-def main():
-    text = read_s3_bucket("mybucket-qwertyui","unstructuredData.txt")
-    text = clean_text(text)
-    response = generate_structured_data(text)
-    DynamoDBBuilder.set_region('us-west-2').connect_to_dynamodb().find_table("dynamodb-test").insert_item(response)
+def main(event, context):
+    CommonUtilities.process_sqs_batch_response(event)
 
 if __name__ == "__main__":
     main()
