d23bae5ee6f635fbe799f57fbaba6dc45b8b249c
kasid Khan <kasidkhan@gmail.com>
2024-12-31 21:42:22 +0100

    modified code for prod deployment

--- src/index.py
+++ src/index.py
@@ -1,10 +1,9 @@
 
 from module.common_utilities import CommonUtilities
 
-def lambda_handler():
+def lambda_handler(event, context):
     util = CommonUtilities()
-    util.process_local_file("C:\\Users\\kasid\\Downloads\\unstructuredData.txt")
-    #CommonUtilities.process_sqs_batch_response(event)
+    util.process_sqs_batch_response(event, context)
 
 if __name__ == "__main__":
     lambda_handler()
