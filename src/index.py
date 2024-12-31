8687ed2a6ac6da6964aaa55ddfd8cc340b442ba9
kasid Khan <kasidkhan@gmail.com>
2024-12-31 21:21:57 +0100

    removed excess colon

--- src/index.py
+++ src/index.py
@@ -3,7 +3,7 @@ from module.common_utilities import CommonUtilities
 
 def lambda_handler():
     util = CommonUtilities()
-    util.process_local_file("C:\\Users\\kasid\\Downloads\\unstructuredData.txt");
+    util.process_local_file("C:\\Users\\kasid\\Downloads\\unstructuredData.txt")
     #CommonUtilities.process_sqs_batch_response(event)
 
 if __name__ == "__main__":
